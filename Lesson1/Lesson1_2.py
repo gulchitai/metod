e12864399488cca0821892a78dc2686b5136464d
gulchitai <gulchitai@bk.ru>
2019-11-14 09:53:25 +1000

    1. Помотреть документацию к API Гитхаба. Разобраться и вывести список всех репозиториев для конкретного пользователя. 2. Выполнить запрос методом GET к ресурсам, использующим любой тип авторизации через Postman, Python.

---
+++ Lesson1/Lesson1_2.py
@@ -0,0 +1,12 @@
+﻿
+import requests
+import json
+main_link = 'https://translate.yandex.net'
+text = 'Hello world'
+appid = ''
+link = f'{main_link}/api/v1.5/tr.json/translate?key={appid}&text={text}&lang=en-ru&format=html'
+req = requests.get(link)
+
+if req.ok:
+    data = json.loads(req.text)
+    print(f"Фраза на английском '{text}' переводится как '{data['text'][0]}'")
